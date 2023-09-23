3aef0299c3f5d722dc3a80e25f0c928d2b5b82d6
Nick Kocharhook <nick@kocharhook.com>
2023-09-24 00:48:28 +0100

    Add title to index page

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,24 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import BLCSMap from 'components/BLCSMap';
 import Link from 'src/Link';
+import Head from 'next/head';
 
 export default function Index() {
   return (
-    <Container maxWidth="md">
-      <Typography variant='h4'>
-        Barnsbury & Laycock Liveable Neighbourhood
-      </Typography>
-      <Typography variant='body1'>
-        Council proposals. Learn more on <Link href="https://www.letstalk.islington.gov.uk/barnsbury-laycock-liveable-neighbourhood">their website</Link>.
-      </Typography>
-      <Box mt={3} xs={12} mb={3}>
-        <BLCSMap />
-      </Box>
-    </Container>
+    <>
+      <Head>
+        <title>Phase 1 — Barnsbury & Laycock</title>
+      </Head>
+      <Container maxWidth="md">
+        <Typography variant='h4'>
+          Barnsbury & Laycock Liveable Neighbourhood
+        </Typography>
+        <Typography variant='body1'>
+          Council proposals. Learn more on <Link href="https://www.letstalk.islington.gov.uk/barnsbury-laycock-liveable-neighbourhood">their website</Link>.
+        </Typography>
+        <Box mt={3} xs={12} mb={3}>
+          <BLCSMap />
+        </Box>
+      </Container></>
   );
 }
